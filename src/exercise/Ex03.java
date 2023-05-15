7eebfd5dd2c1543e604c715bb48a64c5e14d107b
ben <shb0801@naver.com>
2023-05-15 11:37:02 +0900

    Ex4 : break 연습 2

--- src/exercise/Ex03.java
+++ src/exercise/Ex03.java
@@ -3,15 +3,15 @@ package exercise;
 public class Ex03 {
 
 	public static void main(String[] args) {
-		// 외부 반복문
-		while () {
-			// 내부 반복문
-			while () {
-				if ( ) {
-					break
-				}
-			}
-		}
+//		// 외부 반복문
+//		while () {
+//			// 내부 반복문
+//			while () {
+//				if ( ) {
+//					break
+//				}
+//			}
+//		}
 	}
 
 }
---
+++ src/exercise/Ex4.java
@@ -0,0 +1,21 @@
+package exercise;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		// 1부터 더했을 때 합이 500이 넘는 자연수는 얼마인가요?
+		int sum = 0;
+		int num;
+		
+		for (num = 0; ; num++) {
+			sum += num;
+			if (sum >= 500) {
+				break;
+			}
+		}
+		
+		System.out.println("합이 500이 넘는 순간 num = " + num);
+		System.out.println("합이 500이 넘는 순간 sum = " + sum);
+	}
+
+}
