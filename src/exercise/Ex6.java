1706b4d1903e430b777712fdb57d7b755972d95e
ben <shb0801@naver.com>
2023-05-15 12:14:17 +0900

    Ex6 : 짝수 단 구구단

---
+++ src/exercise/Ex6.java
@@ -0,0 +1,21 @@
+package exercise;
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+		// 구구단을 짝수 단만 출력하도록 프로그램을 만들어 보세요. 
+		int num1;
+		int num2;
+		
+		for (num1 = 1; num1 <= 9; num1++) {
+			if ((num1 % 2) != 0) {
+				continue;
+			}
+			for (num2 = 1; num2 <= 9; num2++) {
+				System.out.println(num1 + " X " + num2 + " = " + (num1 * num2));
+			}
+		}
+
+	}
+
+}
