2070668250b3e8c4ac97fa11a7e7abc7e405273f
ben <shb0801@naver.com>
2023-05-15 12:24:09 +0900

    Ex7 : 단보다 곱하는 수가 작거나 같은 경우까지만 출력

---
+++ src/exercise/Ex7.java
@@ -0,0 +1,24 @@
+package exercise;
+
+public class Ex7 {
+
+	public static void main(String[] args) {
+		// 구구단을 단보다 곱하는 수가 작거나 같은 경우까지만 출력하는 프로그램을 만들어 보세요
+		// break 문 사용
+		
+		int num1;
+		int num2;
+		
+		
+		for (num1 = 1; num1 <= 9; num1++) {
+			for (num2 = 1; ; num2++) {
+				System.out.println(num1 + " X " + num2 + " = " + (num1 * num2) + " 입니다.");
+				if (num2 >= num1) {
+					break;
+				}
+			}
+		}
+
+	}
+
+}
