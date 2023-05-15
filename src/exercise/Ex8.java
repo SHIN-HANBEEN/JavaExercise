ed46a6cc75d544cbca9a83dd59bf30d7e78f15cb
ben <shb0801@naver.com>
2023-05-15 13:50:43 +0900

    Ex8 : pyramid 출력하기 수정 2

--- src/exercise/Ex8.java
+++ src/exercise/Ex8.java
@@ -4,22 +4,20 @@ public class Ex8 {
 	
 	public static void main(String[] args) {
 		// 반복문을 사용하여 피라미드 모양을 출력하는 프로그램을 만들어 보세요.
-		int lineCount = 8;  
-		int starCount = 1;
-					
-		for(int i = 1; i<=lineCount; i++) {
-			int spaceCount = (lineCount-i);
-			for(int j = 0; j<spaceCount; j++) {
-				System.out.print(' ');
+		int lineCounter = 8;
+		int starCounter = 1;
+		for (int i=1; i <=lineCounter; i++) {
+			int spaceCounter = lineCounter - i;
+			for (int j=1; j<=spaceCounter; j++) {
+				System.out.print(" ");
 			}
-			for(int j=0; j<starCount; j++) {
-				System.out.print('*');
+			for (int j=1; j<=starCounter; j++) {
+				System.out.print("*");
 			}
-			for(int j = 0; j<spaceCount; j++) {
-				System.out.print(' ');
+			for (int j=1; j<=spaceCounter; j++) {
+				System.out.print(" ");
 			}
-			spaceCount-=1;
-			starCount+=2;
+			starCounter += 2;
 			System.out.println();
 		}
 	}
