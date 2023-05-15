ab1e77a6fad01d7fc47750fd40989e1ef4c1e2aa
ben <shb0801@naver.com>
2023-05-15 18:13:01 +0900

    Ex9 : 마름모로 별 출력

--- src/exercise/Ex8.java
+++ src/exercise/Ex8.java
@@ -4,20 +4,20 @@ public class Ex8 {
 	
 	public static void main(String[] args) {
 		// 반복문을 사용하여 피라미드 모양을 출력하는 프로그램을 만들어 보세요.
-		int lineCounter = 8;
-		int starCounter = 1;
-		for (int i=1; i <=lineCounter; i++) {
-			int spaceCounter = lineCounter - i;
-			for (int j=1; j<=spaceCounter; j++) {
+		int lineCount = 10;
+		int starCount = 1;
+		for (int i=1; i <=lineCount; i++) {
+			int spaceCount = lineCount - i;
+			for (int j=1; j<=spaceCount; j++) {
 				System.out.print(" ");
 			}
-			for (int j=1; j<=starCounter; j++) {
+			for (int j=1; j<=starCount; j++) {
 				System.out.print("*");
 			}
-			for (int j=1; j<=spaceCounter; j++) {
+			for (int j=1; j<=spaceCount; j++) {
 				System.out.print(" ");
 			}
-			starCounter += 2;
+			starCount += 2;
 			System.out.println();
 		}
 	}
---
+++ src/exercise/Ex9.java
@@ -0,0 +1,90 @@
+package exercise;
+
+public class Ex9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int lineCount = 10;
+		int starCount = 1;
+		int h = lineCount/2;
+		boolean isActivated =false;
+		
+		for (int i=1; i <=lineCount; i++) {
+			if ((lineCount % 2)!=0) {
+				if (i<lineCount/2+1) { // 별이 증가하는 부분
+					int spaceCount = lineCount - i;
+					for (int j=1; j<=spaceCount; j++) {
+						System.out.print(" ");
+					}
+					for (int j=1; j<=starCount; j++) {
+						System.out.print("*");
+					}
+					for (int j=1; j<=spaceCount; j++) {
+						System.out.print(" ");
+					}
+					starCount += 2;
+					System.out.println();
+				} 
+				else { // 별이 감소하는 부분
+					if (!isActivated) {
+				        // Code to be executed only once
+						starCount -= 2;
+						h += 2;
+				        isActivated = true;
+				    }
+					starCount -= 2;
+					
+					for (int j=1; j<=h; j++) {
+						System.out.print(" ");
+					}
+					for (int j=1; j<=starCount; j++) {
+						System.out.print("*");
+					}
+					for (int j=1; j<=h; j++) {
+						System.out.print(" ");
+					}					
+					System.out.println();
+					h+=1;
+				}
+			}
+			else {
+				if (i<lineCount/2) { // 별이 증가하는 부분
+					int spaceCount = lineCount - i;
+					for (int j=1; j<=spaceCount; j++) {
+						System.out.print(" ");
+					}
+					for (int j=1; j<=starCount; j++) {
+						System.out.print("*");
+					}
+					for (int j=1; j<=spaceCount; j++) {
+						System.out.print(" ");
+					}
+					starCount += 2;
+					System.out.println();
+				} 
+				else { // 별이 감소하는 부분
+					if (!isActivated) {
+				        // Code to be executed only once
+						starCount -= 2;
+						h += 2;
+				        isActivated = true;
+				    }
+					starCount -= 2;
+					
+					for (int j=1; j<=h; j++) {
+						System.out.print(" ");
+					}
+					for (int j=1; j<=starCount; j++) {
+						System.out.print("*");
+					}
+					for (int j=1; j<=h; j++) {
+						System.out.print(" ");
+					}					
+					System.out.println();
+					h+=1;
+				}
+			}
+		}
+	}
+
+}
