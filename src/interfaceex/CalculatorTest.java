3eadf841fe02778d2611a251c863bb16922c40e2
ben <shb0801@naver.com>
2023-06-04 21:27:37 +0900

    ~ 인터페이스 요소 까지 완료

--- src/interfaceex/CalculatorTest.java
+++ src/interfaceex/CalculatorTest.java
@@ -12,6 +12,9 @@ public class CalculatorTest {
 		System.out.println(calc.times(num1, num2));
 		System.out.println(calc.divide(num1, num2));
 		calc.showInfo();
+		calc.description();  // 디폴트 메서드 + 디폴트 메서드에 포함된 private 메서드 실행됨
+		int[] arr = {1, 2, 3, 4, 5};
+		Calc.total(arr);  // default(정적, static) 메서드에서 private static 메서드 실행
 		
 	}
 
